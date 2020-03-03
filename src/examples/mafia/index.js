78ad8e18a3a2f179bae401b9e732481c74a5cc79
Joseph Sandmeyer <joey.sandmeyer@gmail.com>
2020-03-02 20:11:32 -0800

    Add event listener for clicking

--- src/examples/mafia/index.js
+++ src/examples/mafia/index.js
@@ -11,6 +11,29 @@ import ExampleBase from '../../ExampleBase';
 import Players from './objects/Players';
 import HUD from './hud/HUD';
 
+document.addEventListener("DOMContentLoaded", init, false);
+var canvas = null;
+function init() {
+  canvas = document.getElementsByTagName("canvas")[0];
+  canvas.addEventListener("mousedown", getPosition, false);
+}
+
+function getPosition(event) {
+  var x = new Number();
+  var y = new Number();
+  if (event.x != undefined && event.y != undefined) {
+    x = event.x;
+    y = event.y;
+  } else { // Firefox method to get the position
+    x = event.clientX + document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+    y = event.clientY + document.body.scrollTop +
+        document.documentElement.scrollTop;
+  }
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+  alert("x: " + x + "  y: " + y);
+}
 
 class Game extends ExampleBase { 
   constructor(props, context) {
